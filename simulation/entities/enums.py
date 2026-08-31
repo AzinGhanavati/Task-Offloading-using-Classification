e02fe8888a1dfe74daa2d4287688d57990566f3c
Azin Ghanavati <azinghanavati2002@gmail.com>
2026-08-31 13:10:24 +0330

    add enums

---
+++ simulation/entities/enums.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from enum import Enum, IntEnum
+
+class NodeKind(str, Enum):
+    VEHICLE = "vehicle"
+    MOBILE_FOG = "mobile_fog"
+    FIXED_FOG = "fixed_fog"
+    EDGE = "edge"
+    CLOUD = "cloud"
+
+class TaskStatus(str, Enum):
+    CREATED = "created"
+    DECIDED = "decided"
+    TRANSMITTING = "transmitting"
+    QUEUED = "queued"
+    RUNNING = "running"
+    COMPLETED = "completed"
+    FAILED = "failed"
+
+class OffloadAction(IntEnum):
+    LOCAL = 0
+    CLOUD = 1
+    CANDIDATE_1 = 2
+    CANDIDATE_2 = 3
+    CANDIDATE_3 = 4
+
+class AdmissionDecision(IntEnum):
+    LOCAL = 0
+    ESCALATE_TO_SDN = 1
